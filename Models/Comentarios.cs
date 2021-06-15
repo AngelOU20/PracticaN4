28f62a1f7226def40432e3bc0db82c4f7d5ddf09
MrDark-GG <Mr.Dark_GG@protonmail.ch>
2021-06-15 17:41:20 -0500

    Model Comentarios

---
+++ Models/Comentarios.cs
@@ -0,0 +1,9 @@
+namespace PracticaN4.Models
+{
+    public class Comentarios
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Comentario {get; set;}
+    }
+}
