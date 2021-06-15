8b242e7eb0f1f48dd943db1dbdf0462da018f9ac
Julio Ucharima <julio_ucharima@usmp.pe>
2021-06-15 17:29:43 -0500

    Meme

---
+++ Models/Memes.cs
@@ -0,0 +1,10 @@
+namespace PracticaN4.Models
+{
+    public class Memes
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Foto {get;set;}
+        public string UserID {get; set; }
+    }
+}
